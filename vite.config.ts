a31882f7f35eb2664e34aa39ac7adb7f8c315848
楊竣宇 <yangjunyu@yangjunyudeMacBook-Air.local>
2023-07-10 11:17:28 +0800

    change project url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/rwd_side_project/",
+  base: "/RWD_Sideproject/",
   plugins: [react()],
 });
